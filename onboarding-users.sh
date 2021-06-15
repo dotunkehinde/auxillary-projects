ff6621b5ffecffa1bd91baec8b5c17cf0231d069
dotunkehinde <dotunkehinde@gmail.com>
2021-06-15 17:57:50 +0100

    Update onboarding-users.sh

--- onboarding-users.sh
+++ onboarding-users.sh
@@ -1,1 +1,17 @@
+#! /usr/bin/bash
+    for i in `more names.csv `                  # Loops through names listed in names.csv
+    do
+        egrep "^$i" /etc/passwd >/dev/null      # Checks to see if username already existed
+        if [ $? -eq 0 ]; then
+                echo "$i already exists!"
+        else
+        echo Adding user $i
+        pass=$(perl -e 'print crypt($ARGV[0], "password")' $i)
+        useradd -m -p "$pass" "$i"              # Adds a new user with a password same as the username
+        echo Added user $i
+        usermod -a -G developers "$i"           # Adds the new user to the developers group
+        mkdir /home/$i/.ssh                     # Creates the .ssh directory for the new user
+        cp /home/ubuntu/.ssh/id_rsa.pub /home/$i/.ssh/authorized_keys           #Copies the public key to the authorized_keys for the ne>
 
+        fi
+    done
